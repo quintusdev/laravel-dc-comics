150d7a49a17d8337076bf8e0bba0d25a19cb1071
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-07-31 14:13:52 +0200

    first commit

---
+++ app/Http/Controllers/DcComicController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\dcComic;
+use Illuminate\Http\Request;
+
+class DcComicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\dcComic  $dcComic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(dcComic $dcComic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\dcComic  $dcComic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(dcComic $dcComic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\dcComic  $dcComic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, dcComic $dcComic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\dcComic  $dcComic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(dcComic $dcComic)
+    {
+        //
+    }
+}
