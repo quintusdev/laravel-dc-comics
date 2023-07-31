9542dde7eb6e621303a9f819882708f371b75c0f
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-07-31 18:19:18 +0200

    [ERROR]in HOME Route non definita

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,14 @@
     <div class="bg-black">
         <div class="container">
             <div class="row py-5 position">
-                @foreach ($comics as $comic)
+                @foreach ($comics as $id => $comic)
                     <div class="col-2 py-3">
-                        <div class="card bg-black">
-                            <img class="dimension" src="{{ $comic->thumb }}">
-                            <p class="text-white">{{ $comic->series }}</p>
-                        </div>
+                        <a href="{{ Route('comics.show', $comic->id) }}">
+                            <div class="card bg-black">
+                                <img class="dimension" src="{{ $comic['thumb'] }}">
+                                <p class="text-white">{{ $comic['series'] }}</p>
+                            </div>
+                        </a>
                     </div>
                 @endforeach
                 <div class="d-flex justify-content-center">
